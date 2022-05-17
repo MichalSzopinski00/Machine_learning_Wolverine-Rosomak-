f1dc0721d7ddac77825daf16b8e79881ddb9fd99
Michał Szopiński <Michal.SZopinski@interia.eu>
2022-05-17 20:20:21 +0200

    initial directories

---
+++ Machine_learning_Wolverine-Rosomak-/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This is simple Machine learning project which will try to predict labels for the testing data, within this project we will try to use different ML models in order to determine which one will be suit better.',
+    author='Jan / Michal',
+    license='',
+)
