9b38d2ce41233d89234ff7eab58a380a157bcff4
Michał Szopiński <49531926+MichalSzopinski00@users.noreply.github.com>
2022-06-26 10:23:43 +0200

    black format

--- Machine_learning_Wolverine-Rosomak-/notebooks/classificators_comparison_H2O.py
+++ Machine_learning_Wolverine-Rosomak-/notebooks/classificators_comparison_H2O.py
@@ -2,11 +2,19 @@ import h2o
 import os
 import pandas as pd
 from h2o.automl import H2OAutoML
+
 h2o.init()
 
 
 def loading_files(file_name):
-    df_path = os.path.join('/dbfs/','FileStore','shared_uploads','michal.szopinski@interia.eu','ML_data', file_name)
+    df_path = os.path.join(
+        "dbfs",
+        "FileStore",
+        "shared_uploads",
+        "michal.szopinski@interia.eu",
+        "ML_data",
+        file_name,
+    )
     return df_path
 
 
@@ -14,15 +22,17 @@ def main():
     train_p = pd.read_pickle(loading_files("features.pkl"))
     features_h2o = h2o.H2OFrame(train_p)
 
-    labels = pd.read_csv(loading_files("train_labels.csv"), names=["Labels"],header=None)
+    labels = pd.read_csv(
+        loading_files("train_labels.csv"), names=["Labels"], header=None
+    )
     target_h2o = h2o.H2OFrame(labels)
 
-    train_pd = pd.concat([train_p, labels],axis=1)
+    train_pd = pd.concat([train_p, labels], axis=1)
     train_f = h2o.H2OFrame(train_pd)
 
-    train, valid = train_f.split_frame(ratios=[.80])
+    train, valid = train_f.split_frame(ratios=[0.80])
 
-    y="Labels"
+    y = "Labels"
     # For binary classification, response should be a factor
     train[y] = train[y].asfactor()
 
@@ -32,13 +42,14 @@ def main():
 
     # View the AutoML Leaderboard
     lb = aml.leaderboard
-    lb.head(rows=lb.nrows)  
+    lb.head(rows=lb.nrows)
 
     print(aml.leader)
 
     perf = aml.leader.model_performance(valid)
 
     perf.F1()
-    
+
+
 if __name__ == "__main__":
     main()
